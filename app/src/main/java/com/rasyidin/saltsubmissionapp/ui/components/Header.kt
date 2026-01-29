9090e198bcefba9ce5b7ae78b9a71d7a345c9496
raflirasyidin <raflirasyidin20@gmail.com>
2026-01-30 01:41:38 +0700

    feat(product-list): Slicing Header Component

---
+++ app/src/main/java/com/rasyidin/saltsubmissionapp/ui/components/Header.kt
@@ -0,0 +1,97 @@
+package com.rasyidin.saltsubmissionapp.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rasyidin.saltsubmissionapp.R
+import com.rasyidin.saltsubmissionapp.ui.theme.Red
+import com.rasyidin.saltsubmissionapp.ui.theme.SaltSubmissionAppTheme
+import com.rasyidin.saltsubmissionapp.ui.theme.firaSans
+
+@Composable
+fun Header(
+    title: String,
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    body: String? = null
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
+            .background(Red)
+            .padding(horizontal = 20.dp, vertical = 32.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(30.dp),
+            tint = Color.White
+        )
+        Spacer(Modifier.width(21.dp))
+        Column(modifier = Modifier.weight(1F)) {
+            Text(
+                text = title,
+                fontFamily = firaSans,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            if (body != null) {
+                Spacer(Modifier.height(5.dp))
+                Text(
+                    text = body,
+                    fontFamily = firaSans,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+private fun HeaderPreview() {
+    SaltSubmissionAppTheme {
+        Header(
+            title = "Product List",
+            icon = ImageVector.vectorResource(R.drawable.ic_mobile)
+        )
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+private fun HeaderPreview2() {
+    SaltSubmissionAppTheme {
+        Header(
+            title = "Product List",
+            icon = ImageVector.vectorResource(R.drawable.ic_mobile),
+            body = "3 Products"
+        )
+    }
+}
--- app/src/main/java/com/rasyidin/saltsubmissionapp/ui/theme/Type.kt
+++ app/src/main/java/com/rasyidin/saltsubmissionapp/ui/theme/Type.kt
@@ -1,30 +1,30 @@
 package com.rasyidin.saltsubmissionapp.ui.theme
 
 import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.rasyidin.saltsubmissionapp.R
 
-val firaSansBold = Font(
-    resId = R.font.fira_sans_bold,
-    style = FontStyle.Normal,
-    weight = FontWeight.Bold
-)
-
-val firaSansMedium = Font(
-    resId = R.font.fira_sans_medium,
-    style = FontStyle.Normal,
-    weight = FontWeight.Medium
-)
-
-val firaSansRegular = Font(
-    resId = R.font.fira_sans_regular,
-    style = FontStyle.Normal,
-    weight = FontWeight.Normal
-)
-
-val firaSansSemiBold = Font(
-    resId = R.font.fira_sans_semi_bold,
-    style = FontStyle.Normal,
-    weight = FontWeight.SemiBold
+val firaSans = FontFamily(
+    Font(
+        resId = R.font.fira_sans_bold,
+        style = FontStyle.Normal,
+        weight = FontWeight.Bold
+    ),
+    Font(
+        resId = R.font.fira_sans_medium,
+        style = FontStyle.Normal,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.fira_sans_regular,
+        style = FontStyle.Normal,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.fira_sans_semi_bold,
+        style = FontStyle.Normal,
+        weight = FontWeight.SemiBold
+    ),
 )
